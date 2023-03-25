57905855df7207b7c095dec753a99d2f8dc7e014
WKFilipeDavi <filipedavigarcia@hotmail.com>
2023-03-25 18:44:58 -0300

    add: JS click events

--- scripts/sections/menu.js
+++ scripts/sections/menu.js
@@ -1,5 +1,16 @@
 function init() {
     console.log('menu');
+    let mobileMenuBtn = document.getElementById('menu__btn');
+    let mobileMenu = document.getElementById('mobile-menu');
+    let mobileMenuClose = document.getElementById('mobile-menu__close-btn');
+
+    mobileMenuBtn.addEventListener('click', function() {
+        mobileMenu.classList.add('mobile-menu--active');
+    })
+
+    mobileMenuClose.addEventListener('click', function() {
+        mobileMenu.classList.remove('mobile-menu--active');
+    })
 }
 
 export default {
