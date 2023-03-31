d63309c1616a62c4a38898057293245387c9f398
WKFilipeDavi <filipedavigarcia@hotmail.com>
2023-03-30 21:43:35 -0300

    update: menu - js for tailwind classes, font lato and more...

---
+++ scripts/util/tailwind.js
@@ -0,0 +1,16 @@
+function init() {
+  tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          'rose-ebony': '#7C5050ff',
+        }
+      }
+    }
+  }
+}
+
+export default {
+  init,
+}
