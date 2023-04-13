703e85957576f1330f4ffd8d41f90a523d943d2b
WKFilipeDavi <filipedavigarcia@hotmail.com>
2023-04-13 14:22:39 -0300

    cesta finished

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 import menu from "./sections/menu.js";
 import tailwind from "./util/tailwind.js";
+import cesta from "./views/cesta.js";
 
 (function () {
   //call functions from modules imported
   //menu.init();
   tailwind.init();
+  cesta.init();
 
 })();
